99ff3b68704c1c92d07882857a707013b314a7dd
Ivan Arcuschin <iarcuschin@gmail.com>
2014-03-28 14:35:03 -0300

    Frist steps: numpy + scipy

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
+import numpy as np
+import scipy as sci
+
+class PerceptronSimple():
+    """Perceptron Simple"""
+    def __init__(self):
+        dataset = np.array([[1,2],[1,2]])
+        train_network(dataset)
+
+    def train_network(dataset, batch=False): 
+        D = np.zeros(datset.shape)
+        print(D.shape)
 
