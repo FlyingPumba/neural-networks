c2eaf28b8895031a50dda44a8a4db21461f5793a
Ivan Arcuschin <iarcuschin@gmail.com>
2014-03-29 00:09:42 -0300

    Basic array stuff

--- main.py
+++ main.py
@@ -4,15 +4,27 @@ import scipy as sci
 class PerceptronSimple():
     """Perceptron Simple"""
     def __init__(self):
-        dataset = np.array([[1,2],[1,2]])
-        print(dataset.shape)
+        dataset = np.array([
+            [[0,0],[0,0]],
+            [[0,1],[1,0]],
+            [[1,0],[1,0]],
+            [[1,1],[1,1]]
+            ])
         self.train_network(dataset)
 
-    def train_network(dataset, batch=False):
-        D = np.zeros((2,3))
-        #dataset.shape
-        #self.D = np.zeros(dataset.shape)
-        #print(D.shape)
+    def train_network(self, dataset, batch=False):
+        print "Data set size is:"
+        print dataset.shape
+        print "\n"
+
+        # how many training patterns do we have ?
+        cant_patterns = dataset.shape[0]
+
+        # create D matrix for batch learning
+        D = np.zeros(dataset.shape)
+
+        for x in range(0, cant_patterns):
+            print "Training pattern %d. Input: %s -> Expected output: %s" % (x, dataset[x,0], dataset[x,1])
 
 def main():
     a = PerceptronSimple()
