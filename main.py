6388fb7e2d6c9f786950f61bd6088811aa924118
Ivan Arcuschin <iarcuschin@gmail.com>
2014-03-30 01:03:09 -0300

    Flexible test set

--- main.py
+++ main.py
@@ -5,26 +5,30 @@ def networkANDOR(lRate, epsilon):
     a = ps(2, 2, lRate, epsilon, data.ANDOR.trainingset)
 
     print "\nTesting the network"
-    for i in range(0,4):
+    cant_patterns = data.ANDOR.testset.shape[0]
+    for i in range(0,cant_patterns):
         a.evaluate(data.ANDOR.testset[i])
 
 def networkAND(lRate, epsilon):
     a = ps(2, 2, lRate, epsilon, data.AND.trainingset)
 
     print "\nTesting the network"
-    for i in range(0,4):
+    cant_patterns = data.AND.testset.shape[0]
+    for i in range(0,cant_patterns):
         a.evaluate(data.AND.testset[i])
 
 def networkOR(lRate, epsilon):
     a = ps(2, 2, lRate, epsilon, data.OR.trainingset)
 
     print "\nTesting the network"
-    for i in range(0,4):
+    cant_patterns = data.OR.testset.shape[0]
+    for i in range(0,cant_patterns):
         a.evaluate(data.OR.testset[i])
 
 def networkXOR(lRate, epsilon):
     a = ps(2, 2, lRate, epsilon, data.XOR.trainingset)
 
     print "\nTesting the network"
-    for i in range(0,4):
+    cant_patterns = data.XOR.testset.shape[0]
+    for i in range(0,cant_patterns):
         a.evaluate(data.XOR.testset[i])
