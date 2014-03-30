7f98b62ad1f7dc8bed23af63fd4ba9e2a09f100e
Ivan Arcuschin <iarcuschin@gmail.com>
2014-03-30 01:08:12 -0300

    Added simpleOCR to main

--- main.py
+++ main.py
@@ -10,15 +10,16 @@ def networkANDOR(lRate, epsilon):
         a.evaluate(data.ANDOR.testset[i])
 
 def networkAND(lRate, epsilon):
-    a = ps(2, 2, lRate, epsilon, data.AND.trainingset)
+    a = ps(2, 1, lRate, epsilon, data.AND.trainingset)
 
     print "\nTesting the network"
     cant_patterns = data.AND.testset.shape[0]
+    print cant_patterns
     for i in range(0,cant_patterns):
         a.evaluate(data.AND.testset[i])
 
 def networkOR(lRate, epsilon):
-    a = ps(2, 2, lRate, epsilon, data.OR.trainingset)
+    a = ps(2, 1, lRate, epsilon, data.OR.trainingset)
 
     print "\nTesting the network"
     cant_patterns = data.OR.testset.shape[0]
@@ -26,9 +27,17 @@ def networkOR(lRate, epsilon):
         a.evaluate(data.OR.testset[i])
 
 def networkXOR(lRate, epsilon):
-    a = ps(2, 2, lRate, epsilon, data.XOR.trainingset)
+    a = ps(2, 1, lRate, epsilon, data.XOR.trainingset)
 
     print "\nTesting the network"
     cant_patterns = data.XOR.testset.shape[0]
     for i in range(0,cant_patterns):
         a.evaluate(data.XOR.testset[i])
+
+def networkSimpleOCR(lRate, epsilon):
+    a = ps(25, 5, lRate, epsilon, data.SimpleOCR.trainingset)
+
+    #print "\nTesting the network"
+    #cant_patterns = data.XOR.testset.shape[0]
+    #for i in range(0,cant_patterns):
+    #    a.evaluate(data.XOR.testset[i])
