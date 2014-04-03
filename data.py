5270970297c255d99c8554d0db50fa25155722d9
Ivan Arcuschin <iarcuschin@gmail.com>
2014-04-03 17:35:36 -0300

    Added bpolar OCR

--- data.py
+++ data.py
@@ -41,32 +41,32 @@ class XOR():
 
 class SimpleOCR():
     trainingset = np.asarray([
-        [l.A, [0,0,0,0,1]], # A -> 1
-        [l.B, [0,0,0,1,0]], # B -> 2
-        [l.C, [0,0,0,1,1]], # C -> 3
-        [l.D, [0,0,1,0,0]], # D -> 4
-        [l.E, [0,0,1,0,1]], # E -> 5
-        [l.F, [0,0,1,1,0]], # F -> 6
-        [l.G, [0,0,1,1,1]], # G -> 7
-        [l.H, [0,1,0,0,0]], # H -> 8
-        [l.I, [0,1,0,0,1]], # I -> 9
-        [l.J, [0,1,0,1,0]], # J -> 10
-        [l.K, [0,1,0,1,1]], # K -> 11
-        [l.L, [0,1,1,0,0]], # L -> 12
-        [l.M, [0,1,1,0,1]], # M -> 13
-        [l.N, [0,1,1,1,0]], # N -> 14
-        [l.O, [0,1,1,1,1]], # O -> 15
-        [l.P, [1,0,0,0,0]], # P -> 16
-        [l.Q, [1,0,0,0,1]], # Q -> 17
-        [l.R, [1,0,0,1,0]], # R -> 18
-        [l.S, [1,0,0,1,1]], # S -> 19
-        [l.T, [1,0,1,0,0]], # T -> 20
-        [l.U, [1,0,1,0,1]], # U -> 21
-        [l.V, [1,0,1,1,0]], # V -> 22
-        [l.W, [1,0,1,1,1]], # W -> 23
-        [l.X, [1,1,0,0,0]], # X -> 24
-        [l.Y, [1,1,0,0,1]], # Y -> 25
-        [l.Z, [1,1,0,1,0]], # Z -> 26
+        [l.A, np.array([0,0,0,0,1])], # A -> 1
+        [l.B, np.array([0,0,0,1,0])], # B -> 2
+        [l.C, np.array([0,0,0,1,1])], # C -> 3
+        [l.D, np.array([0,0,1,0,0])], # D -> 4
+        [l.E, np.array([0,0,1,0,1])], # E -> 5
+        [l.F, np.array([0,0,1,1,0])], # F -> 6
+        [l.G, np.array([0,0,1,1,1])], # G -> 7
+        [l.H, np.array([0,1,0,0,0])], # H -> 8
+        [l.I, np.array([0,1,0,0,1])], # I -> 9
+        [l.J, np.array([0,1,0,1,0])], # J -> 10
+        [l.K, np.array([0,1,0,1,1])], # K -> 11
+        [l.L, np.array([0,1,1,0,0])], # L -> 12
+        [l.M, np.array([0,1,1,0,1])], # M -> 13
+        [l.N, np.array([0,1,1,1,0])], # N -> 14
+        [l.O, np.array([0,1,1,1,1])], # O -> 15
+        [l.P, np.array([1,0,0,0,0])], # P -> 16
+        [l.Q, np.array([1,0,0,0,1])], # Q -> 17
+        [l.R, np.array([1,0,0,1,0])], # R -> 18
+        [l.S, np.array([1,0,0,1,1])], # S -> 19
+        [l.T, np.array([1,0,1,0,0])], # T -> 20
+        [l.U, np.array([1,0,1,0,1])], # U -> 21
+        [l.V, np.array([1,0,1,1,0])], # V -> 22
+        [l.W, np.array([1,0,1,1,1])], # W -> 23
+        [l.X, np.array([1,1,0,0,0])], # X -> 24
+        [l.Y, np.array([1,1,0,0,1])], # Y -> 25
+        [l.Z, np.array([1,1,0,1,0])], # Z -> 26
         ])
     testset = trainingset
 
@@ -102,10 +102,13 @@ class SimpleOCR():
 class BinaryOCR(SimpleOCR):
     # Idem to SimpleOCR
     trainingset = SimpleOCR.trainingset
-    #testset = trainingset
+    testset = trainingset
 
 class BipolarOCR(SimpleOCR):
     # Swithc 0s for -1s
-    trainingset = SimpleOCR.trainingset
-    #trainingset[trainingset == 0] = -1
-    #testset = trainingset
+    trainingset = np.copy(SimpleOCR.trainingset)
+    cant_patterns = trainingset.shape[0]
+    for i in range(0, cant_patterns):
+        trainingset[i,0] = SimpleOCR.trainingset[i,0]*2-1
+        trainingset[i,1] = SimpleOCR.trainingset[i,1]*2-1
+    testset = trainingset
