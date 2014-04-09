c71518ee8024fb4d8ddf4f831a2df1b6faaba367
Ivan Arcuschin <iarcuschin@gmail.com>
2014-04-09 19:05:09 -0300

    Speed up perceptron multi

--- perceptronmulti.py
+++ perceptronmulti.py
@@ -10,10 +10,9 @@ class PerceptronMulti():
         self.nHiddenNodes = array_cant_hidden_nodes
         self.nHiddenLayers = len(array_cant_hidden_nodes)
         self.lRate = learning_rate
-        self.dataset = trainingset
         self.epsilon = epsilon
 
-        self.train_network(self.dataset)
+        self.train_network(trainingset)
 
     def train_network(self, dataset, batch=False, stochastic=True):
         print "Data set size is:"
@@ -23,64 +22,70 @@ class PerceptronMulti():
         cant_patterns = dataset.shape[0]
 
         # create D matrix for batch learning
-        self.D = np.zeros(dataset.shape)
+        D = np.zeros(dataset.shape)
 
         # create the Weight matrix (nInput+1 for the threshold)
-        self.W = []
-        self.W.append(np.random.uniform(-0.1,0.1,size=(self.nInput+1, self.nHiddenNodes[0])))
+        W = []
+        W.append(np.random.uniform(-0.1,0.1,size=(self.nInput+1, self.nHiddenNodes[0])))
         for i in xrange(1, self.nHiddenLayers, 1):
-            self.W.append(np.random.uniform(-0.1,0.1,size=(self.nHiddenNodes[i-1], self.nHiddenNodes[i])))
-        self.W.append(np.random.uniform(-0.1,0.1,size=(self.nHiddenNodes[self.nHiddenLayers-1], self.nOutput)))
+            W.append(np.random.uniform(-0.1,0.1,size=(self.nHiddenNodes[i-1], self.nHiddenNodes[i])))
+        W.append(np.random.uniform(-0.1,0.1,size=(self.nHiddenNodes[self.nHiddenLayers-1], self.nOutput)))
 
         cant_epochs = 0
         max_epochs = 1000
 
         self.errors_in_each_epoch = []
-        self.errors_in_each_pattern = []
+        self.appendEpochError = self.errors_in_each_epoch.append
 
         while True:
             # begin epoch
             print "The %d epoch has begun \n" % cant_epochs
-            self.errors_in_each_pattern = []
+            errors_in_each_pattern = []
+            appendPatternError = errors_in_each_pattern.append
 
             # stochastic learning
             if(stochastic):
-                np.random.shuffle(self.dataset)
+                np.random.shuffle(dataset)
 
             for i in xrange(cant_patterns):
                 print "Training pattern %d" % i
-                self.X = self.getInputWithThreshold(dataset[i,0])
-                self.Y = self.evaluate(self.X)
-                print "Output is: %s" % self.Y[-1]
+                X = self.getInputWithThreshold(dataset[i,0])
+                Y = self.evaluate(X, W)
+                print "Output is: %s" % Y[-1]
 
                 print "Expected output is: %s" % dataset[i,1]
                 Z = dataset[i,1]
 
-                self.G = self.backPropagation(self.Y,Z)
+                E = Z-Y[-1]
+                pattern_error = np.dot(E, E)
+                appendPatternError(pattern_error)
+
+                G = self.backPropagation(Y,Z, W)
 
                 # learn !
                 if(batch):
-                    self.D = self.updateWeights(self.W,self.G,self.lRate)
+                    D = self.updateWeights(W,G,self.lRate)
                 else:
-                    self.W = self.updateWeights(self.W,self.G,self.lRate)
+                    W = self.updateWeights(W,G,self.lRate)
             
             if(batch):
-                self.W = self.W + self.D
+                W = W + D
 
             cant_epochs = cant_epochs + 1
-            self.errors_in_each_epoch.append(np.max(self.errors_in_each_pattern))
+            self.appendEpochError(np.max(errors_in_each_pattern))
             
             keep_going = True
             if(cant_epochs >= max_epochs):
                 print "\nREACHED MAX EPOCHS\n"
                 keep_going = False
-            if(self.epsilon >= np.max(self.errors_in_each_pattern)):
+            if(self.epsilon >= np.max(errors_in_each_pattern)):
                 print "\nREACHED BETTER ERROR THAN EPSILON IN LAST EPOCH\n"
                 keep_going = False
 
             if(keep_going == False):
+                self.W = W
                 print "total epochs = %d\n" % cant_epochs
-                print "last e = %.10f\n" % np.max(self.errors_in_each_pattern)
+                print "last e = %.10f\n" % np.max(errors_in_each_pattern)
                 break
 
         print "Final weight matrix is: \n%s\n" % self.W
@@ -96,35 +101,29 @@ class PerceptronMulti():
             plt.xlabel("pattern number")
             plt.show()
 
-    def evaluate(self, input):
+    def evaluate(self, input, weights):
         Y = []
-        Y.append(self.activation(np.dot(self.X,self.W[0])))
+        append = Y.append
+        append(np.tanh(np.dot(input,weights[0])))
         for i in xrange(1, self.nHiddenLayers, 1):
-            Y.append(self.activation(np.dot(Y[i-1],self.W[i])))
-        Y.append(self.activation(np.dot(Y[self.nHiddenLayers-1],self.W[self.nHiddenLayers])))
-        #print "%s" % Y
+            append(np.tanh(np.dot(Y[i-1],weights[i])))
+        append(np.tanh(np.dot(Y[self.nHiddenLayers-1],weights[self.nHiddenLayers])))
+
         return Y
 
-    def activation(self, Y):
-        # use hiperbolic tangent
-        return np.tanh(Y)
-
-    def backPropagation(self, Y, Z):
+    def backPropagation(self, Y, Z, weights):
         # calculate the error
         E = Z-Y[-1]
 
-        pattern_error = np.dot(E, E)
-        self.errors_in_each_pattern.append(pattern_error)
-
         L = self.nHiddenLayers
         G = []
-
+        append = G.append
         for i in xrange(L, 0, -1):
             transposedInput = Y[i-1].reshape(Y[i-1].shape+(1,))
-            d = (1 - (np.tanh(np.dot(Y[i-1],self.W[i])))**2)
+            d = (1 - (np.tanh(np.dot(Y[i-1],weights[i])))**2)
             aux = d * transposedInput * E 
-            G.append(aux)
-            E = np.dot(aux,self.W[i].T)
+            append(aux)
+            E = np.dot(aux,weights[i].T)
         return G
 
     def updateWeights(self, W, G, eta):
@@ -148,9 +147,9 @@ class PerceptronMulti():
 
         for i in xrange(cant_patterns):
             print "\nTesting pattern %d" % (i+1)
-
-            Y = self.evaluate(testset[i,0])
-            print "Output is: %s" % self.Y[-1]
+            X = self.getInputWithThreshold(testset[i,0])
+            Y = self.evaluate(X, self.W)
+            print "Output is: %s" % Y[-1]
             
             print "Expected output is: %s" % testset[i,1]
             Z = testset[i,1]
