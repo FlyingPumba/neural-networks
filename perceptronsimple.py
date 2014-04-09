de6d2376503ef82bc3d0fc2a1ec15c8836ef7b87
Ivan Arcuschin <iarcuschin@gmail.com>
2014-04-09 18:33:04 -0300

    Fixed test error

--- perceptronsimple.py
+++ perceptronsimple.py
@@ -24,7 +24,7 @@ class PerceptronSimple():
         self.W = np.random.uniform(-0.1,0.1,size=(self.nInput+1, self.nOutput))
 
         cant_epochs = 0
-        max_epochs = 10000
+        max_epochs = 1000
 
         self.errors_in_each_epoch = []
         self.appendEpochError = self.errors_in_each_epoch.append        
@@ -113,7 +113,7 @@ class PerceptronSimple():
             self.evaluate()
 
             print "Expected output is: %s" % testset[i,1]
-            Z = testset[i,1]
+            self.Z = testset[i,1]
 
             # calculate the error
             E = self.Z - self.Y
