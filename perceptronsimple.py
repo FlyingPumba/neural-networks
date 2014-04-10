0ae3827995cb327e4f2b919feeea740aa262b752
Ivan Arcuschin <iarcuschin@gmail.com>
2014-04-09 23:58:37 -0300

    100 epochs are good enough for perceptron simple

--- perceptronsimple.py
+++ perceptronsimple.py
@@ -23,7 +23,7 @@ class PerceptronSimple():
         W = np.random.uniform(-0.1,0.1,size=(self.nInput+1, self.nOutput))
 
         cant_epochs = 0
-        max_epochs = 1000
+        max_epochs = 100
 
         self.errors_in_each_epoch = []
         self.appendEpochError = self.errors_in_each_epoch.append        
