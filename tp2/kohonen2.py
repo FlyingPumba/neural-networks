e0830be0a00b5c38cfd173653b892b75953540c8
Ivan Arcuschin <iarcuschin@gmail.com>
2014-05-25 20:21:52 -0300

    Kohonen part 2 - basics

---
+++ tp2/kohonen2.py
@@ -0,0 +1,197 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+import random as rnd
+
+class Region():
+    """Clase que representa una region de puntos"""
+    def __init__(self, minX, maxX, minY, maxY):
+        self.minX = minX
+        self.maxX = maxX
+        self.minY = minY
+        self.maxY = maxY
+
+    def pertenece(self, x, y):
+        return self.minX <= x <= self.maxX and self.minY <= y <= self.maxY
+
+class NoSupervisedNetwork():
+    """Mapas auto-organizados de Kohonen - Parte 2"""
+    def __init__(self):
+        self.nInput = 2
+        self.nOutput = [10,10]
+        self.etaAlpha = 0.1
+        self.sigmaAlpha  = 0.01
+
+    def trainNetwork(self, dataset, stochastic=True):
+        W = np.random.uniform(-0.1,0.1,size=(self.nOutput[0], self.nInput, self.nOutput[1]))
+
+        cant_epochs = 1
+        max_epochs = 50
+
+        while cant_epochs <= max_epochs:
+            # begin a new epoch
+            print cant_epochs
+
+            # stochastic learning
+            if(stochastic):
+                trainingset = np.copy(dataset)
+                np.random.shuffle(trainingset)
+
+            for X in trainingset:
+                Y = self.activation(X,W)
+                P = self.winner(Y)
+                D = self.proxy(P, self.sigma(cant_epochs))
+                dW = []
+                for i in xrange(len(W)):
+                    aux = self.eta(cant_epochs) * (np.array([X]).T - W[i]) * D[i]
+                    dW.append(aux)
+                W = W + dW
+
+            cant_epochs = cant_epochs + 1
+
+        self.W = W
+
+    def activation(self, X, W):
+        Y = []
+        for i in xrange(len(W)):
+            Yaux = (W[i] - np.array([X]).T)**2
+            Yaux = np.apply_along_axis(np.linalg.norm, 0, Yaux)
+            Y.append(Yaux)
+
+        Y = np.array(Y).flatten()
+        return [True if x == min(Y) else False for x in Y]
+
+    def eta(self, t):
+        return t**(-self.etaAlpha)
+
+    def sigma(self, t):
+        return t**(-self.sigmaAlpha)
+
+    def winner(self, Y):
+        indice = Y.index(True)
+        i = indice / self.nOutput[0]
+        j = indice % self.nOutput[1]
+
+        return [i,j]
+
+    def proxy(self, p, sigma):
+        d = np.zeros((self.nOutput[0],self.nOutput[1]))
+        for i in xrange(self.nOutput[0]):
+            for j in xrange(self.nOutput[1]):
+                normaAlCuadrado = (p[0] - i)**2 + (p[1] - j)**2
+                d[i,j] = np.exp( (-normaAlCuadrado/ 2*sigma**2) )
+            
+        return d
+
+    def plotWeights(self):
+        plt.xlabel("Final weights")
+        plt.imshow(self.W,interpolation='none', cmap=cm.gray)
+        plt.show()
+
+    def rotular(self, dataset, regiones):
+        act = np.zeros((self.nOutput[0], self.nOutput[1], len(regiones)))
+        for X in dataset:
+            Y = self.activation(X,self.W)
+            p = self.winner(Y)
+
+            # veo de que region es el punto
+            for i in xrange(len(regiones)):
+                if regiones[i].pertenece(X[0],X[1]):
+                    act[p[0],p[1],i] += 1
+
+        reg = np.zeros((self.nOutput[0], self.nOutput[1]))
+        for i in xrange(self.nOutput[0]):
+            for j in xrange(self.nOutput[1]):
+                reg[i,j] =  np.where(act[i,j] == np.max(act[i,j]))[0][0]
+                if not(np.all(act[i,j] == 0)):
+                    reg[i,j] += 1
+
+        colormap = np.array(['black','r', 'g', 'b', 'y'])
+        points = []
+        for i in xrange(self.nOutput[0]):
+            for j in xrange(self.nOutput[1]):
+                points.append([i,j])
+
+        aux = zip(*points)
+        plt.title("Mapa de Regiones")
+        plt.scatter(aux[0], aux[1], s=100, c=colormap[reg.flatten().astype(np.int64)])
+        plt.show()
+
+    def validateNetwork(self, validationset):
+        frecuencias = [0]*self.nOutput
+        for X in validationset:
+            Y = self.activation(X,self.W)
+            indice = Y.index(True)
+            frecuencias[indice] += 1
+
+        pos = np.arange(self.nOutput)
+        width = 1.0     # gives histogram aspect to the bar diagram
+        # show label for all bins
+        ax = plt.axes()
+        ax.set_xticks(pos + (width / 2))
+        ax.set_xticklabels(pos)
+
+        plt.bar(pos, frecuencias, width, color='r')
+        plt.title("Frecuencias de Activacion")
+        plt.xlabel("Neurona")
+        plt.ylabel("Frecuencia")
+        plt.show()
+
+# ========== DATASET ==========
+
+def generateDataset(cant):
+    dataset = []
+    # create the regions
+    r1 = Region(10,30,10,30)
+    r2 = Region(40,60,10,30)
+    r3 = Region(10,30,40,60)
+    r4 = Region(40,60,40,60)
+
+    for i in xrange(cant):
+        # choose a region
+        r = np.random.randint(4)
+
+        if r == 0:
+            x = np.random.uniform(r1.minX,r1.maxX)
+            y = np.random.uniform(r1.minY,r1.maxY)
+            dataset.append(np.array([x,y]))
+        elif r == 1:
+            x = np.random.uniform(r2.minX,r2.maxX)
+            y = np.random.uniform(r2.minY,r2.maxY)
+            dataset.append(np.array([x,y]))
+        elif r == 2:
+            x = np.random.uniform(r3.minX,r3.maxX)
+            y = np.random.uniform(r3.minY,r3.maxY)
+            dataset.append(np.array([x,y]))
+        elif r == 3:
+            x = np.random.uniform(r4.minX,r4.maxX)
+            y = np.random.uniform(r4.minY,r4.maxY)
+            dataset.append(np.array([x,y]))
+
+    return np.array(dataset)
+
+# ========== MAIN ==========
+# numpy print options
+np.set_printoptions(suppress=True)
+np.set_printoptions(precision=5)
+
+if __name__ == "__main__":
+    net = NoSupervisedNetwork()
+
+    # generate the data and validation sets
+    cant_patterns_training = 200
+    #cant_patterns_validation = 400
+
+    data = generateDataset(cant_patterns_training)
+    
+    net.trainNetwork(data)
+    print "Final weights: %s" % net.W
+
+    r1 = Region(10,30,10,30)
+    r2 = Region(40,60,10,30)
+    r3 = Region(10,30,40,60)
+    r4 = Region(40,60,40,60)
+    net.rotular(data, [r1,r2,r3,r4])
+
+    #net.validateNetwork(normal_valset)
+    #net.plotWeights()
