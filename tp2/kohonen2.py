307677aea00815d9573bb9f9d2cdcb6472887416
Ivan Arcuschin <iarcuschin@gmail.com>
2014-05-28 20:33:54 -0300

    Improved kohonen2 plots

--- tp2/kohonen2.py
+++ tp2/kohonen2.py
@@ -20,21 +20,21 @@ class NoSupervisedNetwork():
     def __init__(self):
         self.nInput = 2
         self.nOutput = [10,10]
-        self.etaAlpha = 0.1
-        self.sigmaAlpha  = 0.4
+        self.etaAlpha = 0.2
+        self.sigmaAlpha  = 0.1
 
         self.etaHistory = []
         self.sigmaHistory = []
 
-    def trainNetwork(self, dataset, stochastic=True):
-        W = np.random.uniform(-0.1,0.1,size=(self.nOutput[0], self.nInput, self.nOutput[1]))
+    def trainNetwork(self, dataset, valset, stochastic=True):
+        self.W = np.random.uniform(-0.1,0.1,size=(self.nOutput[0], self.nInput, self.nOutput[1]))
 
         cant_epochs = 1
-        max_epochs = 50
+        max_epochs = 20
 
         while cant_epochs <= max_epochs:
             # begin a new epoch
-            print "Epoch: %d, eta: %.8f, sigma: %.8f" % (cant_epochs, self.eta(cant_epochs), self.sigma(cant_epochs))
+            #print "Epoch: %d, eta: %.8f, sigma: %.8f" % (cant_epochs, self.eta(cant_epochs), self.sigma(cant_epochs))
 
             self.etaHistory.append(self.eta(cant_epochs))
             self.sigmaHistory.append(self.sigma(cant_epochs))
@@ -45,21 +45,20 @@ class NoSupervisedNetwork():
                 np.random.shuffle(trainingset)
 
             for X in trainingset:
-                Y = self.activation(X,W)
+                Y = self.activation(X,self.W)
                 P = self.winner(Y)
                 D = self.proxy(P, self.sigma(cant_epochs))
                 #print D
                 dW = []
-                for i in xrange(len(W)):
-                    aux = self.eta(cant_epochs) * (np.array([X]).T - W[i]) * D[i]
+                for i in xrange(len(self.W)):
+                    aux = self.eta(cant_epochs) * (np.array([X]).T - self.W[i]) * D[i]
                     dW.append(aux)
-                W = W + dW
+                self.W = self.W + dW
 
-            cant_epochs = cant_epochs + 1
-            #print "Epoch: %d, eta: %.8f, sigma: %.8f" % (cant_epochs, self.eta(cant_epochs), self.sigma(cant_epochs))
+            print "Epoch: %d, eta: %.8f, sigma: %.8f" % (cant_epochs, self.eta(cant_epochs), self.sigma(cant_epochs))
+            #self.rotularYValidar(dataset,valset, [r1,r2,r3,r4])
             #raw_input()
-
-        self.W = W
+            cant_epochs = cant_epochs + 1
 
     def activation(self, X, W):
         Y = []
@@ -94,8 +93,31 @@ class NoSupervisedNetwork():
         return d
 
     def plotWeights(self):
-        plt.xlabel("Final weights")
-        plt.imshow(self.W,interpolation='none', cmap=cm.gray)
+        plt.title("Final weights")
+        points = self.W.reshape(self.nOutput[0],self.nOutput[1],self.nInput)
+        auxX = []
+        auxY = []
+        for i in xrange(self.nOutput[0]):
+            for j in xrange(self.nOutput[1]):
+                auxX.append(points[i,j,0])
+                auxY.append(points[i,j,1])
+
+        sp =plt.subplot(111)
+        # regiones en el dataset
+        if separados:
+            sp.add_patch(patch.Rectangle((10,10),20,20, fill=False, color='r'))
+            sp.add_patch(patch.Rectangle((40,10),20,20, fill=False, color='g'))
+            sp.add_patch(patch.Rectangle((10,40),20,20, fill=False, color='b'))
+            sp.add_patch(patch.Rectangle((40,40),20,20, fill=False, color='y'))
+        else:
+            sp.add_patch(patch.Rectangle((10,10),20,20, fill=False, color='r'))
+            sp.add_patch(patch.Rectangle((30,10),20,20, fill=False, color='g'))
+            sp.add_patch(patch.Rectangle((10,30),20,20, fill=False, color='b'))
+            sp.add_patch(patch.Rectangle((30,30),20,20, fill=False, color='y'))
+
+        plt.scatter(auxX, auxY, s=50)
+        fileName = "kohonen2-weights-%d" % filekey
+        plt.savefig(fileName, bbox_inches='tight')
         plt.show()
 
     def rotularYValidar(self, dataset, validationset, regiones):
@@ -130,8 +152,7 @@ class NoSupervisedNetwork():
         #plt.subplot2grid((2,2), (0,0))
         plt.subplot(121)
         plt.title("Mapa de Regiones")
-        plt.scatter(aux[0], aux[1], s=100, c=colormap[reg.flatten().astype(np.int64)])
-        #plt.legend(loc='upper center', bbox_to_anchor=(0.5, 1.05), ncol=3, fancybox=True)
+        scat = plt.scatter(aux[0], aux[1], s=100, c=colormap[reg.flatten().astype(np.int64)])
 
         # dataset
         #sp = plt.subplot2grid((2,2), (0,1))
@@ -169,11 +190,16 @@ class NoSupervisedNetwork():
         p2 = patch.Rectangle((0, 0), 1, 1, fc="g")
         labelWrong = 'Wrong points: %d' % correcto.count(0)
         labelRight = 'Right points: %d' % correcto.count(1)
-        plt.legend((p1, p2), (labelWrong,labelRight), loc='upper center', ncol=3)
+        plt.legend((p1, p2), (labelWrong,labelRight), loc='upper center', fancybox=True)
 
-        fileName = 'kohonen2-%d.png' % np.random.randint(1000)
+        supt = "Parametros: eta: %.2f sigma-alpha: %.2f" % (self.etaAlpha, self.sigmaAlpha)
+        plt.suptitle(supt)
+        fileName = 'kohonen2-%d.png' % filekey
         print fileName
-        plt.savefig(fileName, bbox_inches='tight')
+        figure = plt.gcf() # get current figure
+        figure.set_size_inches(10, 8) #this will give us a 800x600 image
+        # when saving, specify the DPI
+        plt.savefig(fileName, bbox_inches='tight', dpi = 100)
         plt.show()
 
 # ========== DATASET ==========
@@ -209,6 +235,8 @@ def generateDataset(cant, plot=False, mismaDensidad=True):
             r = r3
         elif regionEspecial%4 == 3:
             r = r4
+
+        r = r2
 
         for i in xrange(cant/2):
             x = np.random.uniform(r.minX,r.maxX)
@@ -253,6 +281,7 @@ def generateDataset(cant, plot=False, mismaDensidad=True):
 np.set_printoptions(suppress=True)
 np.set_printoptions(precision=5)
 
+filekey = np.random.randint(1000)
 separados = True
 if separados:
     r1 = Region(10,30,10,30)
@@ -266,6 +295,7 @@ else:
     r4 = Region(30,50,30,50)
 
 if __name__ == "__main__":
+    #plt.ion()
     net = NoSupervisedNetwork()
 
     # generate the data and validation sets
@@ -274,11 +304,11 @@ if __name__ == "__main__":
 
     data = generateDataset(cant_patterns_training, plot=False, mismaDensidad=True)
 
-    validation = generateDataset(cant_patterns_training)
+    validation = generateDataset(cant_patterns_training, plot=False, mismaDensidad=True)
 
-    net.trainNetwork(data)
+    net.trainNetwork(data, validation)
     print "Final weights: %s" % net.W
 
     net.rotularYValidar(data, validation, [r1,r2,r3,r4])
 
-    #net.plotWeights()
+    net.plotWeights()
