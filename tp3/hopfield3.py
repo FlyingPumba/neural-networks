3b33c8beca59930021bacaf9779758dd2d8f7d8f
Ivan Arcuschin <iarcuschin@gmail.com>
2014-06-23 18:53:53 -0300

    Added estocastic activation to hopfield3

--- tp3/hopfield3.py
+++ tp3/hopfield3.py
@@ -7,7 +7,7 @@ import ocrletters as l
 import random as rnd
 
 class HopfieldNetwork():
-    """Memoria asociativa de Hopfield - Parte 2 - Ejercicio ?"""
+    """Memoria asociativa de Hopfield - Parte 2 - Ejercicio 3"""
     def __init__(self):
         self.cantNeuronas = 100
 
@@ -24,7 +24,7 @@ class HopfieldNetwork():
     def sigmoid(self, x, temp) :
         return 1/(1 + np.exp( -x / temp ))
 
-    def activate(self, X, temp, synch=False):
+    def activate(self, X, temp, synch=False, plotEnergy=False):
         S = X
         Saux = np.zeros(self.cantNeuronas)
         Eh = []
@@ -43,11 +43,66 @@ class HopfieldNetwork():
                     S[i] = np.sign(self.sigmoid(np.dot(S, self.W[:,i]), temp) - np.random.uniform(0,1))
 
             E = self.energy(S,self.W)
-            #print "E: %s" % E
+            print "E: %s" % E
             Eh.append(E)
-            # self.plotEnergy(Eh)
-            # show(E,S)
+            if plotEnergy:
+                self.plotEnergy(Eh)
+        if plotEnergy:
+            raw_input()
+        plt.ioff()
         return S
+
+    def activatewd(self, X, temp, desiredMem, dist, hamming = False, synch=False):
+        S = X
+        Saux = np.zeros(self.cantNeuronas)
+        Eh = []
+        plt.ion()
+       
+        if hamming = False :
+            while(np.sqrt(numpy.sum((desiredMem - Saux)**2)) > dist) :
+               
+                if synch:
+                    S = np.sign(S*self.W)
+                else:
+                    I = np.random.permutation(self.cantNeuronas)
+                    for i in I:
+                        #print "I: %s" % S
+                        #S[i] = np.sign(np.dot(S, self.W[:,i]))
+                        S[i] = np.sign(self.sigmoid(np.dot(S, self.W[:,i]), temp) - np.random.uniform(0,1))
+ 
+                E = self.energy(S,self.W)
+                #print "E: %s" % E
+                Eh.append(E)
+                # self.plotEnergy(Eh)
+                # show(E,S)
+                Saux = np.copy(S)
+            return S
+        else :
+            while(distHamming(desiredMem, Saux) > dist) :
+ 
+                if synch:
+                    S = np.sign(S*self.W)
+                else:
+                    I = np.random.permutation(self.cantNeuronas)
+                    for i in I:
+                        #print "I: %s" % S
+                        #S[i] = np.sign(np.dot(S, self.W[:,i]))
+                        S[i] = np.sign(self.sigmoid(np.dot(S, self.W[:,i]), temp) - np.random.uniform(0,1))
+ 
+                E = self.energy(S,self.W)
+                #print "E: %s" % E
+                Eh.append(E)
+                # self.plotEnergy(Eh)
+                # show(E,S)
+                Saux = np.copy(S)
+            return S
+   
+    def distHamming(a, b) :
+        dist = 0
+        for i in xrange(len(a)) :
+            if a[i] != b[i]:
+                dist += 1
+        return dist
 
     def plotWeights(self):
         plt.xlabel("Final weights")
@@ -55,6 +110,7 @@ class HopfieldNetwork():
         plt.show()
 
     def plotEnergy(self, energyHistory):
+        plt.clf()
         plt.xlabel('Iteracion')
         plt.ylabel('Energia')
         plt.plot(energyHistory)
@@ -66,33 +122,15 @@ np.set_printoptions(suppress=True)
 np.set_printoptions(precision=5)
 
 if __name__ == "__main__":
-    #plt.ion()
     net = HopfieldNetwork()
 
     # generate the memories
-    memories = d.OCR.trainingset
-    
-    a = np.random.choice([-1, 1], 100)
-    b = a
-    indexUsado = []
+    cantMemorias = 10
+    memories = []
+ 
+    while cantMemorias > 0 :
+        memories.append(np.round(np.random.sample(100)) * 2 - 1)
+        cantMemorias -= 1
 
-    cota = a.sum()
-    print cota
-
-    if cota > 0 :
-        while cota > 0 :
-            indice = np.random.randint(100)
-            if a[indice] == -1 and not indice in indexUsado :
-                b[indice] = -a[indice]
-                cota -= 1
-                indexUsado.append(indice)
-    else :
-        while cota < 0 :
-            indice = np.random.randint(100)
-            if a[indice] == 1 and not indice in indexUsado :
-                b[indice] = -a[indice]
-                cota += 1
-                indexUsado.append(indice)
-    
     # sets the temperature
     temp = 0.4
