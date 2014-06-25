3d15de4ddafc4df2c62b0e3707aea8195e40f3d3
McCoy2X <federicobeuter@gmail.com>
2014-06-25 15:01:59 -0300

    Functional version of hopfield3.py

--- tp3/hopfield3.py
+++ tp3/hopfield3.py
@@ -24,22 +24,22 @@ class HopfieldNetwork():
     def sigmoid(self, x, temp) :
         return 1/(1 + np.exp( -x / temp ))
 
-    def activate(self, X, temp, dist, hamming = False, plotEnergy=False):
+    def activate(self, X, memories, temp, dist, hamming = False, plotEnergy=False):
         S = X
         Saux = np.zeros(self.cantNeuronas)
         Eh = []
         plt.ion()
 
-        currentDist = self.dist(S, Saux, hamming)
+        currentDist = min(self.dist(Saux, i) > dist for i in memories)
         print currentDist
 
-        while currentDist > dist:
+        while all(self.dist(Saux, i) > dist for i in memories):
             Saux = np.copy(S)
             I = np.random.permutation(self.cantNeuronas)
             for i in I:
                 S[i] = np.sign(self.sigmoid(np.dot(S, self.W[:,i]), temp) - np.random.uniform(0,1))
 
-            currentDist = self.dist(S, Saux, hamming)
+            currentDist = min(self.dist(Saux, i) > dist for i in memories)
             print currentDist
 
             E = self.energy(S,self.W)
@@ -53,7 +53,7 @@ class HopfieldNetwork():
             raw_input()
 
         plt.ioff()
-        return S
+        return [i for i in memories if self.dist(Saux, i) <= dist][0]
 
     def dist(self, a, b, hamming=False):
         if hamming:
@@ -90,12 +90,19 @@ np.set_printoptions(precision=5)
 
 if __name__ == "__main__":
     net = HopfieldNetwork()
+    
+    def ham(a, b): #Lo copie para probar una cosa
+        dist = 0
+        for i in xrange(len(a)) :
+            if a[i] != b[i]:
+                dist += 1
+        return dist
 
     # temperature
     temp = 0.4
 
     # threshold distance to consider one pattern the same as other
-    tDist = 0.01
+    tDist = 5
 
     # generate the memories
     cantMemorias = 10
@@ -113,8 +120,12 @@ if __name__ == "__main__":
     # test that for all the memories, the ouput of the activation is the same
     print "\n VALIDATION with original memories\n"
     for X in memories:
-        output = net.activate(np.copy(X), temp, tDist,  plotEnergy=True)
+        output = net.activate(np.copy(X), memories, temp, tDist, plotEnergy=False)
         if (output == X).all():
             print "RIGHT memory"
+            print output
         else:
             print "WRONG memory"
+            print output
+            print X
+            print ham(output, X)
